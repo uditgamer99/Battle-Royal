0d0caf8c3068655942e87e30726726782361b662
uditgamer99 <uditkhanna700@gmail.com>
2025-07-13 00:40:14 +0530

    Delete BRNetworkManager.cs

--- BRNetworkManager.cs
+++
@@ -1,7 +0,0 @@
-using Mirror;
-
-public class BRNetworkManager : NetworkManager {
-    public override void OnServerDisconnect(NetworkConnection conn) {
-        base.OnServerDisconnect(conn);
-    }
-}
